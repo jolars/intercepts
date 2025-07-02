2a340f2f1dc864a79e1b8fc80a117294ded29fdf
Johan Larsson <jolars@posteo.com>
2025-07-02 15:38:17 +0200

    Reformat and fix exact update

--- src/intercept_strategies.jl
+++ src/intercept_strategies.jl
@@ -1,3 +1,5 @@
+using Statistics
+
 abstract type InterceptStrategy end
 
 struct NoIntercept <: InterceptStrategy end
@@ -5,15 +7,33 @@ struct GradientStrategy <: InterceptStrategy end
 struct NewtonStrategy <: InterceptStrategy end
 struct ExactStrategy <: InterceptStrategy end
 
-function update_intercept(::NoIntercept, f::LossFunction, intercept::Real, η::AbstractVector{<:Real}, y::AbstractVector{<:Real})
+function update_intercept(
+  ::NoIntercept,
+  f::LossFunction,
+  intercept::Real,
+  η::AbstractVector{<:Real},
+  y::AbstractVector{<:Real},
+)
   0
 end
 
-function update_intercept(::GradientStrategy, f::LossFunction, intercept::Real, η::AbstractVector{<:Real}, y::AbstractVector{<:Real})
+function update_intercept(
+  ::GradientStrategy,
+  f::LossFunction,
+  intercept::Real,
+  η::AbstractVector{<:Real},
+  y::AbstractVector{<:Real},
+)
   return intercept - sum(gradient(f, η, y)) / (f.lipschitz * length(η))
 end
 
-function update_intercept(::NewtonStrategy, f::LossFunction, intercept::Real, η::AbstractVector{<:Real}, y::AbstractVector{<:Real})
+function update_intercept(
+  ::NewtonStrategy,
+  f::LossFunction,
+  intercept::Real,
+  η::AbstractVector{<:Real},
+  y::AbstractVector{<:Real},
+)
   grad = sum(gradient(f, η, y))
   hess = sum(hessian(f, η, y))
 
@@ -21,27 +41,37 @@ function update_intercept(::NewtonStrategy, f::LossFunction, intercept::Real, η
 end
 
 # Iteratively update the intercept until convergence
-function update_intercept(::ExactStrategy, f::LossFunction, intercept::Real, η::AbstractVector{<:Real}, y::AbstractVector{<:Real})
+function update_intercept(
+  ::ExactStrategy,
+  f::LossFunction,
+  intercept::Real,
+  η::AbstractVector{<:Real},
+  y::AbstractVector{<:Real},
+)
   η_copy = copy(η)
+  n = length(η)
 
-  max_it = 15
+  max_it = 20
 
   for _ in 1:max_it
     grad = sum(gradient(f, η_copy, y))
 
-    if abs(grad) < 1e-14
-      return intercept
+    if abs(grad) < 1e-10
+      break
     end
 
     hess = sum(hessian(f, η_copy, y))
 
-    new_intercept = intercept - grad / hess
+    if abs(hess) < 1e-10
+      # If Hessian is small, use standard gradient descent step
+      new_intercept = intercept - grad / (f.lipschitz * n)
+    else
+      new_intercept = intercept - grad / hess
+    end
 
     η_copy .+= new_intercept - intercept
 
     intercept = new_intercept
-
-    abs_grad = abs(grad)
   end
 
   return intercept
